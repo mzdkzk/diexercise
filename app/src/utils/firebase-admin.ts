72af18f7b584659ad610704fa4d3ccb3a9651df6
mzdkzk <mzdkzk@yahoo.co.jp>
2020-06-29 00:33:19 +0900

    Cloud Runへのデプロイ

--- app/src/utils/firebase-admin.ts
+++ app/src/utils/firebase-admin.ts
@@ -3,9 +3,17 @@ import fs from 'fs'
 import path from 'path'
 
 if (!admin.apps.length) {
-  const serviceAccountKey = fs.readFileSync(
-    path.join(process.env.ROOT!, 'serviceAccountKey.json')
-  )
+  let serviceAccountKey: Buffer | undefined
+  try {
+    serviceAccountKey = fs.readFileSync(
+      path.join(process.env.ROOT!, 'serviceAccountKey.json')
+    )
+  } catch (e) {
+    serviceAccountKey = Buffer.from(
+      process.env.FIREBASE_SA_KEY_BASE64!,
+      'base64'
+    )
+  }
   admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(serviceAccountKey.toString())),
     databaseURL: 'https://live-reference.firebaseio.com'
