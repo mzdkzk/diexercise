b47a74fd929f9726cf09843cd71a4ceca6ec5aba
mzdkzk <mzdkzk@yahoo.co.jp>
2020-07-04 01:07:13 +0900

    部屋を動的ルートに移動

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,100 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { Word, Button, Layout } from '../components'
-import firebase from '../utils/firebase'
-import request from '../utils/request'
-import { WordData } from '../scheme/db'
+import React from 'react'
+import { Button, Layout } from '../components'
+import Link from 'next/link'
 
-const getRecognition = (): SpeechRecognition | null => {
-  try {
-    const SpeechRecognition =
-      (window as any).webkitSpeechRecognition ||
-      (window as any).SpeechRecognition
-    const recognition = new SpeechRecognition()
-    recognition.lang = 'ja-JP'
-    recognition.interimResults = true
-    recognition.continuous = true
-    return recognition
-  } catch (e) {
-    return null
-  }
-}
-
-export const IndexPage: React.FC = () => {
-  const [storedWords, setStoredWords] = useState<string[]>([''])
-
-  let currentWordId: string | undefined
-  let mediaStream: MediaStream | undefined
-
-  const recognition = getRecognition()
-  const db = firebase.firestore()
-  const wordsRef = db.collection('rooms').doc('test').collection('words')
-
-  useEffect(() => {
-    const unsubscribe = wordsRef.orderBy('updatedAt').onSnapshot(snapshot => {
-      setStoredWords(snapshot.docs.map(doc => (doc.data() as WordData).text))
-    })
-    return () => {
-      stopRecord()
-      unsubscribe()
-    }
-  }, [])
-
-  const start = async () => {
-    if (!recognition) {
-      alert('このブラウザは音声入力に対応していません')
-      return
-    }
-    mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: false,
-      audio: true
-    })
-    recognition.addEventListener('result', async e => {
-      const result = e.results[e.resultIndex][0].transcript
-      if (e.results[e.resultIndex].isFinal) {
-        request.post(`/test/${currentWordId}`, {
-          json: { text: result }
-        })
-        currentWordId = undefined
-      } else {
-        if (!currentWordId) {
-          currentWordId = `${+new Date()}`
-        }
-        wordsRef.doc(currentWordId).set(
-          {
-            text: result,
-            updatedAt: new Date()
-          },
-          { merge: true }
-        )
-      }
-    })
-    recognition.start()
-  }
-
-  const stopRecord = () => {
-    if (recognition) {
-      recognition.stop()
-    }
-    if (mediaStream) {
-      mediaStream.getTracks().forEach(track => track.stop())
-      mediaStream = undefined
-    }
-  }
-
-  const deleteData = () => {
-    request.delete('/test')
-  }
-
+const IndexPage: React.FC = () => {
+  const onChangeHandler = () => {}
   return (
     <Layout>
-      <Button onClick={start}>Start</Button>
-      <Button onClick={stopRecord}>Stop</Button>
-      <Button onClick={deleteData}>Delete</Button>
-      <div>
-        {storedWords.map((word, i) => (
-          <Word key={i}>{word}</Word>
-        ))}
-      </div>
+      <h1>トークルームを作成</h1>
+      <input onChange={onChangeHandler} placeholder="管理者名を入力" />
+      <Button>作成</Button>
+      <Link href="/rooms/[roomId]" as="/rooms/test">
+        <a>テスト用ルーム</a>
+      </Link>
     </Layout>
   )
 }
