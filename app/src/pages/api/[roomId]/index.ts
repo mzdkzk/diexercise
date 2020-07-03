1b0d29e832ba79a20af03e08f57cbcc41e22e869
mzdkzk <mzdkzk@yahoo.co.jp>
2020-07-04 00:17:00 +0900

    部屋作成用API追加

--- app/src/pages/api/[roomId]/index.ts
+++ app/src/pages/api/[roomId]/index.ts
@@ -4,21 +4,39 @@ import admin from '../../../utils/firebase-admin'
 import { cors } from '../../../utils/api-middleware'
 
 const handler: NextApiHandler<ApiData> = async (req, res) => {
-  await cors(req, res, { methods: ['DELETE'] })
-
+  await cors(req, res, { methods: ['POST', 'DELETE'] })
   const db = admin.firestore()
-  const wordsRef = db
-    .collection('rooms')
-    .doc(`${req.query.roomId}`)
-    .collection('words')
 
-  wordsRef.get().then(snapshot => {
+  const handlePOST = async () => {
+    const roomRef = await db.collection('rooms').add({
+      ownerName: req.body.ownerName,
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    })
+    res.json({ isSuccess: true, body: { roomId: roomRef.id } })
+  }
+
+  const handleDELETE = async () => {
+    const wordsRef = db
+      .collection('rooms')
+      .doc(`${req.query.roomId}`)
+      .collection('words')
+
+    const snapshot = await wordsRef.get()
     const batch = db.batch()
     snapshot.docs.forEach(doc => batch.delete(doc.ref))
-    batch.commit()
-  })
+    await batch.commit()
 
-  res.json({ isSuccess: true })
+    res.json({ isSuccess: true })
+  }
+
+  switch (req.method) {
+    case 'POST':
+      await handlePOST()
+      break
+    case 'DELETE':
+      await handleDELETE()
+      break
+  }
 }
 
 export default handler
