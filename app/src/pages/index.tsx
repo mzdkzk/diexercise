0bff128fd4b10fd7ead6df5e0b45479fa2941308
mzdkzk <mzdkzk@yahoo.co.jp>
2020-07-22 11:07:45 +0900

    ルーム作成時に匿名ログインするように

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -7,17 +7,21 @@ import { ApiData } from '../scheme/api'
 import { Form, Input } from '../components/forms'
 import { Layout } from '../components/layouts'
 import { PageFC } from '../scheme/pages'
+import firebase from '../utils/firebase'
 
 const IndexPage: PageFC = () => {
   const [ownerName, setOwnerName] = useState<string>('')
+  const [isLoading, setLoading] = useState<boolean>(false)
   const onChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
     setOwnerName(e.currentTarget.value)
   }
   const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setLoading(true)
     const response = await request.post('/api/rooms', {
       json: { ownerName }
     })
+    await firebase.auth().signInAnonymously()
     const responseJson: ApiData = await response.json()
     Router.push('/rooms/[roomId]', `/rooms/${responseJson.body!.roomId}`)
   }
@@ -30,7 +34,7 @@ const IndexPage: PageFC = () => {
           placeholder="ユーザー名を入力してルームを作成"
           required
         />
-        <Button type="submit">作成</Button>
+        <Button type="submit">{isLoading ? '作成中...' : '作成'}</Button>
       </Form>
       <Link href="/rooms/[roomId]" as="/rooms/test">
         <a>テスト用ルーム</a>
