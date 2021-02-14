588fb44c4209b72b88a4758433fe95f328fc36c6
mzdkzk <mzdkzk@yahoo.co.jp>
2021-02-14 22:25:08 +0900

    単語検索をWikipediaのAPIに変更

--- app/src/components/Reference.tsx
+++ app/src/components/Reference.tsx
@@ -5,17 +5,35 @@ type Props = {
   word: string
 }
 
+type WikipediaResponse = {
+  query: {
+    pages: {
+      [key: number]: {
+        extract?: string
+      }
+    }
+  }
+}
+
 const Reference: React.FC<Props> = ({ word }) => {
   const [reference, setReference] = useState<string>('')
 
   useEffect(() => {
     if (word === '') return
     const search = async () => {
-      const response = await request.get(`/api/reference?word=${word}`)
-      const responseJson = await response.json()
-      if (responseJson.body) {
-        setReference(responseJson.body.text)
-      }
+      const response = await request.get(
+        `https://ja.wikipedia.org/w/api.php?` +
+          `format=json&` +
+          `action=query&` +
+          `prop=extracts&` +
+          `origin=*&` +
+          `explaintext&` +
+          `exintro&` +
+          `titles=${word}`
+      )
+      const responseJson: WikipediaResponse = await response.json()
+      const page = Object.values(responseJson.query.pages)[0]
+      setReference(page.extract || '[単語の意味が見つかりませんでした]')
     }
     search()
   }, [word])
